c33c707213f999ae4d840cb6bf3496a3f9b401bc
Nahidul Isalm <60182620+reflection4046@users.noreply.github.com>
2020-02-17 20:14:04 +0600

    getter setter

---
+++ Student.java
@@ -0,0 +1,38 @@
+package lab4;
+
+public class Student 
+
+{
+   int ID;
+   String Name;
+   
+public int getID() 
+{
+	return ID;
+}
+
+public void setID(int iD) 
+
+{
+	ID = iD;
+}
+
+public String getName() 
+
+{
+	return Name;
+}
+
+public void setName(String name)
+
+{
+	Name = name;
+}
+
+public String toString() 
+{
+	return "Student [ID=" + ID + ", Name=" + Name + "]";
+}
+
+
+}
