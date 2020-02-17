4181f0b0d3ca3c0cece9da014d2967025f9bde6f
Nahidul Isalm <60182620+reflection4046@users.noreply.github.com>
2020-02-17 20:11:34 +0600

    test class

---
+++ CalculatorTest.java
@@ -0,0 +1,23 @@
+package lab3;
+
+public class CalculatorTest {
+
+public static void main(String[] args) 
+	
+	{
+		Calculator C1 = new Calculator (144.00,12.00);
+		
+		System.out.println(C1);
+		
+		System.out.println(" addition : " +C1.add());
+		
+		System.out.println(" Subtraction  : " +C1.sub());
+		
+		System.out.println(" Multiplication : " +C1.multi());
+		
+		System.out.println(" Divison  : " +C1.div());
+		
+	
+
+	}
+}
