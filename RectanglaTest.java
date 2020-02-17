5afbcca1c31edfd87e9e73d272edb14a591405bc
Nahidul Isalm <60182620+reflection4046@users.noreply.github.com>
2020-02-17 20:13:07 +0600

    Test class

---
+++ RectanglaTest.java
@@ -0,0 +1,14 @@
+package lab3;
+
+public class RectanglaTest {
+
+	public static void main(String[] args) {
+		Rectangle R1 = new Rectangle( 34.00,40.00);
+		System.out.println(R1);
+		
+		System.out.println(" area  : " +R1.CalArea());
+				
+
+	}
+
+}
