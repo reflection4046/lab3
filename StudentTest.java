8205a3e4fffeed89599c883847d582efc240bc30
Nahidul Isalm <60182620+reflection4046@users.noreply.github.com>
2020-02-17 20:14:44 +0600

    Testclass

---
+++ StudentTest.java
@@ -0,0 +1,22 @@
+package lab4;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+Student s1= new Student();
+        
+        s1.setID(300);
+        s1.setName("Nahid");
+        
+        System.out.println(s1);
+        
+        
+        Student s2= new Student();
+        
+        s2.setID(5000);
+        s2.setName("IslAM");
+        
+        System.out.println(s1);
+	}
+
+}
